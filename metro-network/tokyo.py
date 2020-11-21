0435c326885204b733c7b40e3b369e55735040b9
MatheusR42 <matheus.araujo.r42@gmail.com>
2020-11-21 13:52:00 -0300

    add networkx

---
+++ metro-network/tokyo.py
@@ -0,0 +1,30 @@
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dfNodes = pd.read_csv('../nodes.csv')
+dfVertices = pd.read_csv('../vertices.csv')
+
+def getNodes():
+  nodes = []
+  for index, row in dfNodes.iterrows():
+    nodes.append((row["Id"], {
+      "color": row["Color"],
+      "LineName": row["LineName"],
+      "Line": row["Line"],
+    }))
+
+  return nodes
+
+def getEdges():
+  edges = []
+  for index, row in dfVertices.iterrows():
+    edges.append((row['Source'], row['Target']))
+
+  return edges
+
+G = nx.Graph()
+G.add_nodes_from(getNodes())
+G.add_edges_from(getEdges())
+nx.draw(G, with_labels=True)
+plt.show()
